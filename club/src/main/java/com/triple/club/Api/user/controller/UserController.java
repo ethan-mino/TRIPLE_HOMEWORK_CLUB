be4acba229316ae5933cdbfad49a246b4cce0526
rlfalsgh95 <rlfalsgh95@naver.com>
2022-06-23 00:41:28 +0900

    Add User Validation

--- club/src/main/java/com/triple/club/Api/user/controller/UserController.java
+++ club/src/main/java/com/triple/club/Api/user/controller/UserController.java
@@ -6,9 +6,12 @@ import com.triple.club.Api.user.service.UserService;
 import com.triple.club.Api.user.vo.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class UserController {
@@ -26,28 +29,33 @@ public class UserController {
     }
 
     @PostMapping("/signup") // 회원가입
-    public ResponseEntity<ApiResponse<User>> signup(@RequestBody User user){
-        String username = user.getUsername();
-        Boolean isUsernameDup = usernameDupValidation(username); // 아이디 중복 검사
-
+    public ResponseEntity<ApiResponse<User>> signup(@Valid @RequestBody User user, BindingResult bindingResult){
         ApiResponse<User> apiResponse = new ApiResponse<>();
 
-        if(!isUsernameDup){  // 아이디가 중복되지 않은 경우
-            String password = user.getPassword();
-            String encodedPassword = passwordEncoder.encode(password);  // 비밀번호 암호화
+        if(bindingResult.hasErrors()){
+            apiResponse.setResult(ApiBasicResult.ERR_INVALID_VALUE);
+        }else{
+            String username = user.getUsername();
+            Boolean isUsernameDup = usernameDupValidation(username); // 아이디 중복 검사
 
-            user.setPassword(encodedPassword);
-            int createCnt = userService.createUser(user);
 
-            if(createCnt == 1){ // 계정이 생성된 경우
-                User createdUser = userService.selectUserByUsername(username);
-                apiResponse.setResult(ApiBasicResult.INF_SUCCESS);
-                apiResponse.setData(createdUser);
-            }else{  // 계정 생성에 실패한 경우
-                apiResponse.setResult(ApiBasicResult.ERR_DB);
+            if(!isUsernameDup){  // 아이디가 중복되지 않은 경우
+                String password = user.getPassword();
+                String encodedPassword = passwordEncoder.encode(password);  // 비밀번호 암호화
+
+                user.setPassword(encodedPassword);
+                int createCnt = userService.createUser(user);
+
+                if(createCnt == 1){ // 계정이 생성된 경우
+                    User createdUser = userService.selectUserByUsername(username);
+                    apiResponse.setResult(ApiBasicResult.INF_SUCCESS);
+                    apiResponse.setData(createdUser);
+                }else{  // 계정 생성에 실패한 경우
+                    apiResponse.setResult(ApiBasicResult.ERR_DB);
+                }
+            }else{  // 아이디가 중복된 경우
+                apiResponse.setResult(ApiBasicResult.ERR_DUP_ENTRY);
             }
-        }else{  // 아이디가 중복된 경우
-            apiResponse.setResult(ApiBasicResult.ERR_DUP_ENTRY);
         }
 
         return ResponseEntity.ok(apiResponse);
--- club/src/main/java/com/triple/club/Api/user/vo/User.java
+++ club/src/main/java/com/triple/club/Api/user/vo/User.java
@@ -2,22 +2,26 @@ package com.triple.club.Api.user.vo;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.Date;
 
 @Getter
 @Setter
 @ToString
-@Builder
 public class User {
     private String id;
 
+    @NotBlank
     private String username;
+
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$\n")
     private String password;
 
     @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
