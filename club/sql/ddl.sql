2c8f77c477152487b279358f943d496b5c75f52c
rlfalsgh95 <rlfalsgh95@naver.com>
2022-06-25 22:55:07 +0900

    Add address properties and unique constraints to the Place table.

--- club/sql/ddl.sql
+++ club/sql/ddl.sql
@@ -10,11 +10,13 @@ CREATE TABLE user (
 CREATE TABLE place (
       id VARCHAR(255) NOT NULL COMMENT 'id',
       name VARCHAR(255) NOT NULL COMMENT '장소 이름',
-      user_id VARCHAR(255) NOT NULL COMMENT 'user 테이블의 id',
+      writer_id VARCHAR(255) NOT NULL COMMENT '장소 생성자 아이디',
+      address VARCHAR(255) NOT NULL COMMENT '주소',
       latitude VARCHAR(255) NOT NULL COMMENT '위도',
       longitude VARCHAR(255) NOT NULL COMMENT '경도',
       created_at DATE NOT NULL COMMENT '생성 일자',
       updated_at DATE NOT NULL COMMENT '수정 일자',
       PRIMARY KEY (id),
-      FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+      UNIQUE KEY `unique_address` (address, latitude, longitude),
+      FOREIGN KEY (writer_id) REFERENCES user(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
